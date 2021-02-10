8ab5509fe21eafed406767752756037d412612fb
yamaguchi722 <yamaguchi.y.0722@gmail.com>
2021-02-10 18:12:17 +0900

    message

---
+++ share-api/config/cors.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+ 'paths' => ['*'],
+ 'allowed_methods' => ['*'],
+ 'allowed_origins' => ['*'],
+ 'allowed_origins_patterns' => [],
+ 'allowed_headers' => ['*'],
+ 'exposed_headers' => false,
+ 'max_age' => false,
+ 'supports_credentials' => false,
+];
+
+?>
